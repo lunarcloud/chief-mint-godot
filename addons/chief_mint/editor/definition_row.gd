36795689b37422563cd9c4be0f7354635f424503
Samuel Sarette <samuelsarette@linux.com>
2023-02-13 21:41:25 -0500

    Improved mint ui for rarity

--- addons/chief_mint/editor/definition_row.gd
+++ addons/chief_mint/editor/definition_row.gd
@@ -8,6 +8,9 @@ var name_edit
 var max_progress_spin_box
 var description_text_edit
 var partial_progress_check_box
+var rarity_options: OptionButton
+var rarity_completion
+var icon_display
 
 signal definition_changed(definition)
 
@@ -23,10 +26,17 @@ func _enter_tree():
 	max_progress_spin_box = $HBoxContainer/InfoContainer/MaxProgressSpinBox
 	description_text_edit = $HBoxContainer/InfoContainer/DescriptionTextEdit
 	partial_progress_check_box = $HBoxContainer/InfoContainer/PartialProgressCheckBox
+	rarity_options = $HBoxContainer/InfoContainer/TopRightArea/RarityOptions
+	rarity_completion = $HBoxContainer/InfoContainer/TopRightArea/CompletionRarity
+	icon_display = $HBoxContainer/IconContainer/ImageDisplay/ImageTexture
 
 
 func set_editor_scale(value: float) -> void:
-	rect_min_size = rect_min_size * value
+	rect_min_size *= value
+	$HBoxContainer/InfoContainer/TopRightArea/CompletionRarity.rect_min_size *= value
+	$HBoxContainer/InfoContainer/TopRightArea/RarityOptions.rect_min_size *= value
+	$HBoxContainer/InfoContainer/TopRightArea/SubtractButton.rect_min_size *= value
+	$HBoxContainer/IconContainer/ImageDisplay.rect_min_size *= value
 
 
 func _on_SubtractButton_pressed():
@@ -38,12 +48,32 @@ func set_definition(def: ChiefMintDefinitionResource) -> void:
 	definition = def
 	if def == null:
 		return
+	
 	if is_instance_valid(name_edit):
 		name_edit.text = def.name 
+		
+	if is_instance_valid(description_text_edit):
+		description_text_edit.text = def.description 
+		
+	if is_instance_valid(icon_display) and ResourceLoader.exists(def.icon_path):
+		icon_display.texture = load(def.icon_path)
+		
 	if is_instance_valid(max_progress_spin_box):
 		max_progress_spin_box.value = def.maximum_progress
+		
 	if is_instance_valid(partial_progress_check_box):
 		partial_progress_check_box.pressed = def.display_partial_progress
+		
+	if is_instance_valid(rarity_completion) and is_instance_valid(rarity_options):
+		rarity_completion.visible = def.rarity == ChiefMintDefinitionResource.ChiefMintRarity.Completion
+		rarity_options.visible = not rarity_completion.visible
+		rarity_options.select(def.rarity)
+		if rarity_completion.visible:
+			$HBoxContainer/InfoContainer/MaxProgressLabel.visible = false
+			max_progress_spin_box.visible = false
+			$HBoxContainer/InfoContainer/DescriptionLabel.visible = false
+			description_text_edit.visible = false
+			partial_progress_check_box.visible = false
 
 
 func _on_NameEdit_text_changed(new_text):
@@ -63,8 +93,11 @@ func _on_DescriptionTextEdit_text_changed():
 
 
 func _on_ImageChangeButton_pressed():
-	pass # Replace with function body.
+	$ImageFileDialog.show_modal()
 
 
 func _on_ImageFileDialog_file_selected(path):
-	pass # Replace with function body.
+	if not ResourceLoader.exists(path):
+		return
+	definition.icon_path = path
+	icon_display.texture = load(path)
--- addons/chief_mint/resources/chief_mint_definition.gd
+++ addons/chief_mint/resources/chief_mint_definition.gd
@@ -1,9 +1,26 @@
 class_name ChiefMintDefinitionResource, "res://addons/chief_mint/icon/icon.svg"
 extends Resource
 
+enum ChiefMintRarity {
+	Common = 0,
+	Uncommon,
+	Rare,
+	Completion
+}
+
+
 export var name: String
+
+export var description: String
+
+export var icon_path: String
+
 export(int, 1, 255, 1) var maximum_progress: int
+
 export var display_partial_progress: bool = true
+
+export(int, "Common", "Uncommon", "Rare", "Completion") var rarity := ChiefMintRarity.Common
+
 
 func _init():
 	self.resource_name = "Chief Mint Definition Resource"
