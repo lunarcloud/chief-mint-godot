56cbfcf86a548bb1471e461cb6786ce5c5bacfa9
Sam Sarette <samuelsarette@linux.com>
2025-12-21 19:09:37 -0500

    cleanup demo UI

--- demo_scene/demo.gd
+++ demo_scene/demo.gd
@@ -21,11 +21,3 @@ func _on_CommonEventButton_pressed():
 func _on_ClearLocalButton_pressed():
 	if not chief_mints.clear_all_progress():
 		OS.alert("Couldn't clear the achievements!", 'Clear Error')
-
-
-func _on_TriggerMultipleButton_pressed():
-	# Trigger multiple achievements in quick succession to test the queue
-	chief_mints.increment_progress("Rare Event")
-	chief_mints.increment_progress("Hat Trick")
-	chief_mints.increment_progress("commonish")
-
